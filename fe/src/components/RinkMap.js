a9562dab05e9dbe2e23a08396f16763688225a77
Luce <lucy_liu@live.ca>
2021-01-04 21:11:29 -0500

    Set up Prettier

--- fe/src/components/RinkMap.js
+++ fe/src/components/RinkMap.js
@@ -1,34 +1,25 @@
-import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React from "react";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
-const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const containerStyle = {
-  width: '500px',
-  height: '500px',
-  margin: '2em',
+  width: "500px",
+  height: "500px",
+  margin: "2em",
 };
 
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
+const center = { lat: 43.74, lng: -79.373 };
 
 function RinkMap() {
   return (
-    <LoadScript
-      googleMapsApiKey={API_KEY}
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
+    <LoadScript googleMapsApiKey={API_KEY}>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={11}>
+        {/* Child components, such as markers, info windows, etc. */}
         <></>
       </GoogleMap>
     </LoadScript>
-  )
+  );
 }
 
-export default React.memo(RinkMap)
+export default React.memo(RinkMap);
